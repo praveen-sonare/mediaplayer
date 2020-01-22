4518cdde5ddb053a4759f7b50f038c53bc41a45e
Marius Vlad <marius.vlad@collabora.com>
2020-01-22 20:30:28 +0200

    app: Remove libhomescreen and windowmanger callbacks
    Removes also depedencies from config.
    
    Signed-off-by: Marius Vlad <marius.vlad@collabora.com>
    Change-Id: I1e455a81a06f5d7d712d09621de585459122edd7

--- app/main.cpp
+++ app/main.cpp
@@ -26,8 +26,7 @@
 #include <QtQml/qqml.h>
 #include <QtQuickControls2/QQuickStyle>
 #include <QQuickWindow>
-#include <libhomescreen.hpp>
-#include <qlibwindowmanager.h>
+
 #include <mediaplayer.h>
 
 #include <unistd.h>
@@ -63,42 +62,11 @@ int main(int argc, char *argv[])
         query.addQueryItem(QStringLiteral("token"), secret);
         bindingAddress.setQuery(query);
         context->setContextProperty(QStringLiteral("bindingAddress"), bindingAddress);
-        std::string token = secret.toStdString();
-        LibHomeScreen* hs = new LibHomeScreen();
-        QLibWindowmanager* qwm = new QLibWindowmanager();
-
-        // WindowManager
-        if(qwm->init(port,secret) != 0){
-            exit(EXIT_FAILURE);
-        }
-        AGLScreenInfo screenInfo(qwm->get_scale_factor());
-        // Request a surface as described in layers.json windowmanager’s file
-        if (qwm->requestSurface(graphic_role) != 0) {
-            exit(EXIT_FAILURE);
-        }
-        // Create an event callback against an event type. Here a lambda is called when SyncDraw event occurs
-        qwm->set_event_handler(QLibWindowmanager::Event_SyncDraw, [qwm, &graphic_role](json_object *object) {
-            fprintf(stderr, "Surface got syncDraw!\n");
-            qwm->endDraw(graphic_role);
-        });
-
-        // HomeScreen
-        hs->init(port, token.c_str());
-        // Set the event handler for Event_ShowWindow which will activate the surface for windowmanager
-        hs->set_event_handler(LibHomeScreen::Event_ShowWindow, [qwm, &graphic_role](json_object *object){
-            qDebug("Surface %s got showWindow\n", graphic_role.toStdString().c_str());
-            qwm->activateWindow(graphic_role);
-        });
 
         context->setContextProperty("AlbumArt", "");
         context->setContextProperty("mediaplayer", new Mediaplayer(bindingAddress, context));
-        context->setContextProperty(QStringLiteral("screenInfo"), &screenInfo);
 
         engine.load(QUrl(QStringLiteral("qrc:/MediaPlayer.qml")));
-        QObject *root = engine.rootObjects().first();
-        QQuickWindow *window = qobject_cast<QQuickWindow *>(root);
-        QObject::connect(window, SIGNAL(frameSwapped()), qwm, SLOT(slotActivateWindow()
-        ));
     }
     return app.exec();
 }
